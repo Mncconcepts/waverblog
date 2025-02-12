51ad9842a155b08b5f32bb16bbd38248e343e187
Mncconcepts <clintonnweze111@gmail.com>
2025-02-12 10:31:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Route, Routes, useLocation, Outlet } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useLocation, Outlet } from "react-router-dom"; // use HashRouter
 import "./App.css";
 import Navitems from "./components/Navitems";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
@@ -27,30 +26,31 @@ function App() {
     });
   }, []);
 
-    return (
-    <div className={`app ${darkMode ? "dark-mode" : "light-mode"}`}>
-      <Routes>
-        {/* Define routes for the application */}
-        <Route path="/" element={<Signup onLogin={handleLogin} />} />
-        <Route path="/signup" element={<Signup onLogin={handleLogin} />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-      </Routes>
+  return (
+    <Router> {/* Wrap everything with Router */}
+      <div className={`app ${darkMode ? "dark-mode" : "light-mode"}`}>
+        <Routes>
+          {/* Define routes for the application */}
+          <Route path="/" element={<Signup onLogin={handleLogin} />} />
+          <Route path="/signup" element={<Signup onLogin={handleLogin} />} />
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        </Routes>
 
-      {/* Render Navbar, Footer, and other components only if not on the signup page */}
-      {!isSignupPage && (
-        <>
-          <Navbar user={user} />
-          <div className="main-container">
-            <div className="content">
-              <Outlet />
+        {/* Render Navbar, Footer, and other components only if not on the signup page */}
+        {!isSignupPage && (
+          <>
+            <Navitems user={user} />
+            <div className="main-container">
+              <div className="content">
+                <Outlet />
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </Router>
   );
 }
-      
 
 export default App;
