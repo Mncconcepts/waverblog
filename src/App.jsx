1af4ee3ee770fe66e39a651083eabd0c60d735ae
Mncconcepts <clintonnweze111@gmail.com>
2025-02-12 10:18:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,30 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <>
-      <Navitems />
-      <div className="min-vh-100">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Outlet />
-      </div>
-      <Footer />
+    return (
+    <div className={`app ${darkMode ? "dark-mode" : "light-mode"}`}>
+      <Routes>
+        {/* Define routes for the application */}
+        <Route path="/" element={<SignUp onLogin={handleLogin} />} />
+        <Route path="/signup" element={<SignUp onLogin={handleLogin} />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+      </Routes>
 
-    </>
+      {/* Render Navbar, Footer, and other components only if not on the signup page */}
+      {!isSignupPage && (
+        <>
+          <Navbar user={user} />
+          <div className="main-container">
+            <div className="content">
+              <Outlet />
+            </div>
+            <Footer />
+          </div>
+        </>
+      )}
+    </div>
   );
 }
+      
 
 export default App;
